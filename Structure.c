25ad294bd849801ffb64f4da73a89fc613474b59
MINA1995 <tsunoda.gitaddress@gmail.com>
2014-08-23 01:56:26 +0900

    Ver03

--- Structure.c
+++ Structure.c
@@ -1,47 +1,50 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #define WRONG 1
 #define GOOD 0
+#define N 100
 
-struct Profil{
+struct Profile{
   char *name;
   int num;
-} p;
+}p[N];
+
+void myPrint(int n){
+  int i;
+  printf("Person's name registration list!");
+  for(i=0;i<n;i++){
+    printf("[%xPerson] Name:%s Age:%x\n",i+1,p[i].name,p[i].num);
+  }
+}
 
 
-void myPrint(char *name,int num){
+void myEnter(int n){
+  int i=0;
+  int num;
+  char name[20];
   
-  p.name=name;
-  p.num=num;
-  printf("NAME:%s AGE:%x\n",p.name,p.num);
+  for(i=0;i<n;i++){
+   
+    printf("name[20 Within]>");
+    scanf("%s",name);
+    
+    printf("Age[120 Within])>");
+    scanf("%x",&num);
+    
+    p[i].name=name;
+    p[i].num=num;
+    
+  }
+  myPrint(n);
 }
-
-
-int myAge(int num){
-  if(num<=0||num>=120){
-    return (num);
-  }else if(num<0||num>120){
-    printf("NO!,return Age enter.");
-    main();
-    }
-}
-
-
 
 int main(void){
-
-  struct Profil p;
-  char name[20];
-  int num;
-
-  printf("you are profil!\n");
-  printf("name[20 Within]>");
-  scanf("%s",name);
-  printf("Age[120 Within])>"); 
-  scanf("%x",&num);
-  
-  myPrint(name,num);
+  int n=0;
+  printf("Register a name!\n");
+  printf("How many people registered?[100 within]>");
+  scanf("%x",&n);
+  myEnter(n);
 
   return(GOOD);
-  
-}
+ }
