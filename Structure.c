dcd28d3745abb0ba509f9ccf80241938e7e941a8
MINA1995 <tsunoda.gitaddress@gmail.com>
2014-08-28 15:57:50 +0900

    Ver04

--- Structure.c
+++ Structure.c
@@ -1,50 +1,37 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> 
 #include <string.h>
-#define WRONG 1
-#define GOOD 0
-#define N 100
+#include <stdlib.h>
 
-struct Profile{
-  char *name;
-  int num;
-}p[N];
-
-void myPrint(int n){
-  int i;
-  printf("Person's name registration list!");
-  for(i=0;i<n;i++){
-    printf("[%xPerson] Name:%s Age:%x\n",i+1,p[i].name,p[i].num);
-  }
-}
-
-
-void myEnter(int n){
-  int i=0;
-  int num;
-  char name[20];
-  
-  for(i=0;i<n;i++){
-   
-    printf("name[20 Within]>");
-    scanf("%s",name);
-    
-    printf("Age[120 Within])>");
-    scanf("%x",&num);
-    
-    p[i].name=name;
-    p[i].num=num;
-    
-  }
-  myPrint(n);
-}
+struct list {
+	int Age;		
+	char Name[20];		
+	struct list *next;      
+};
 
 int main(void){
-  int n=0;
-  printf("Register a name!\n");
-  printf("How many people registered?[100 within]>");
-  scanf("%x",&n);
-  myEnter(n);
+  struct list *p;
+  struct list *head = NULL;	
+  char name[20];
+  int age = 0;
 
-  return(GOOD);
- }
+  printf("yoour profile\n");
+  printf("Name>");
+  scanf("%s",name);
+  printf("age>");
+  scanf("%x",&age);
+  
+  p =(struct list *)malloc(sizeof(struct  list));
+  
+  
+  strcpy(p->Name,name);
+  p->Age=age;
+  
+  p->next=head;
+  head = p;
+   
+  printf("%s %x\n",p->Name,p->Age);
+  p=p->next;
+  return(0);
+}
+
+
