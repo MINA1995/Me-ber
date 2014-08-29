1dc0906b16b49cdf979e3a7e1e8cc3f40e7574ef
MINA1995 <tsunoda.gitaddress@gmail.com>
2014-08-29 09:13:04 +0900

    Ver05

--- Structure.c
+++ Structure.c
@@ -3,36 +3,49 @@
 #include <stdlib.h>
 
 struct list {
-	int Age;		
-	char Name[20];		
-	struct list *next;      
+  int Age;		
+  int N;
+  char Name[20];	
+  struct list *next;      
 };
 
+
+void myPrintf(struct list  *p){
+ 
+  while(p !=NULL){
+    printf("Name:[%s]Age:[%x]\n",p->Name,p->Age);
+  p=p->next;
+  }
+
+}
 int main(void){
   struct list *p;
   struct list *head = NULL;	
   char name[20];
   int age = 0;
+  int f=1;
+  int n=0;
 
-  printf("Name-of-a-person registration!\n");
-  
-  printf("The input of a name(Unril 20)>");
-  scanf("%s",name);
-  printf("The input of age>");
-  scanf("%x",&age);
- 
- 
-  p =(struct list *)malloc(sizeof(struct  list));
-  
-  
-  strcpy(p->Name,name);
-  p->Age=age;
-  
-  p->next=head;
-  head = p;
-   
-  printf("%s %x\n",p->Name,p->Age);
-  p=p->next;
+  printf("Name-of-a-person registration!\n"); 
+
+  while(f){
+    printf("The input of a name(Unril 20)>");
+    scanf("%s",name);
+    printf("The input of age>");
+    scanf("%x",&age);  
+    p =(struct list *)malloc(sizeof(struct  list));
+    if(age<1){
+      f=0;
+    }else{
+      strcpy(p->Name,name);
+      p->Age=age;
+      ++n;
+      p->N=n;
+    }
+   p->next=head;
+   head = p;
+  }   
+  myPrintf(head);
   return(0);
 }
 
